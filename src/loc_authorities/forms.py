5bab4ce4612ff665ad9cccb3543f91daf1215a6e
David Ragnar Nelson <drnelson6@gmail.com>
2026-06-29 14:51:53 -0400

    Try a different approach for the widget

--- src/loc_authorities/forms.py
+++ src/loc_authorities/forms.py
@@ -1,16 +1,16 @@
 from dal import autocomplete
 
 
+class LocWidget(autocomplete.ListAlight):
+    pass
+
+
 class LocField(autocomplete.AlightListCreateChoiceField):
     """Wrapper for dal field as it requires a predefined choice-list, which
-    for this use-case is always empty.
+    for this use case is always empty.
     """
     choice_list = []
-    widget = autocomplete.ListAlight(url='suggest')
+    widget = LocWidget()
 
     def __init__(self, choice_list=choice_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-
-class LocWidget(autocomplete.ListAlight):
-    url = 'suggest'
